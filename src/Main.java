bc15b750d4a0b90a45b4e18a491a49689407d3b0
J <equus497@gmail.com>
2023-12-16 10:53:46 -0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String firstName = "Max";
+        String lastName = "Rockatansky";
+        String address = "66 W. Fury Road";
+        String city = "New South Wales";
+        int zipCode = 2609;
+
+        System.out.println("First name: " + firstName);
+        System.out.println("Last name: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("Zip code: " + zipCode);
+    }
+}
